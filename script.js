49881028660bff3d4911ab4fd409c3d3a26708f1
Salihkaymakamoglu <kaymakamoglusalih@gmail.com>
2023-07-02 17:50:56 +0300

    ppppppp

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let sections = document.querySelectorAll("section");
 let navLinks = document.querySelectorAll("header nav a");
-
 let menuIcon = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
+
+
 menuIcon.onclick = () => {
   menuIcon.classList.toggle("bx-x");
   navbar.classList.toggle("active");
